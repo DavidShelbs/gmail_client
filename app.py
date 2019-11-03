bbcf5295f220f2d9786d4139e1156b4c48919e19
dnsnwb <dnsnwb@mst.edu>
2019-11-03 16:07:37 -0600

    working

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 import quickstart
 import oauth2
 import build_service
+import os
 
-RECIPIENT = ""
+# session['RECIPIENT'] = session['MESSAGE'] = session['SUBJECT'] = ""
 
 app = Flask(__name__)
 
@@ -14,8 +15,9 @@ def home():
 
 @app.route('/spam', methods=['GET', 'POST'])
 def spam():
-    global RECIPIENT
-    RECIPIENT = request.form.get('to')
+    session['RECIPIENT'] = request.form.get('to')
+    session['SUBJECT'] = request.form.get('subject')
+    session['MESSAGE'] = request.form.get('message')
     return redirect(oauth2.get_authorization_url("dnshelby712@gmail.com", None))
     # scope = "https://mail.google.com/"
     # if GOOGLE_REFRESH_TOKEN is None:
@@ -32,17 +34,17 @@ def spam():
 
 @app.route('/done', methods=['GET', 'POST'])
 def done():
-    global RECIPIENT
     code = request.args.get('code')
     creds = oauth2.get_credentials(code, None)
     user_info = oauth2.get_user_info(creds)
     email_address = user_info.get('email')
     gmail = build_service.build_service(creds)
-    message = quickstart.create_message("me", RECIPIENT, "Test subject", "Test body")
+    message = quickstart.create_message("me", session['RECIPIENT'], session['SUBJECT'], session['MESSAGE'])
     quickstart.send_message(gmail, "me", message)
     return render_template('index.html')
 
 
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
     app.run(debug=True,host='192.168.0.17', port=5001,ssl_context='adhoc')
