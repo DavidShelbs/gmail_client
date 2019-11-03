a6028caf8020f35ea3d936df1ab1505df843b2c2
dnsnwb <dnsnwb@mst.edu>
2019-11-03 05:16:59 -0600

    working

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
-# import quickstart
-import email_client
+import quickstart
+import oauth2
+import build_service
+
+RECIPIENT = ""
 
 app = Flask(__name__)
-
-message = """\
-Subject: Hi there
-
-This message is sent from Python."""
 
 @app.route('/')
 def home():
@@ -16,13 +14,35 @@ def home():
 
 @app.route('/spam', methods=['GET', 'POST'])
 def spam():
-    # quickstart.main()
-    email = request.form.get('from')
-    to = request.form.get('to')
-    password = request.form.get('password')
-    email_client.send_message(email, to, message, password)
+    global RECIPIENT
+    RECIPIENT = request.form.get('to')
+    return redirect(oauth2.get_authorization_url("dnshelby712@gmail.com", None))
+    # scope = "https://mail.google.com/"
+    # if GOOGLE_REFRESH_TOKEN is None:
+    #     return redirect(gmail.generate_permission_url(GOOGLE_CLIENT_ID, scope))
+        # session['refresh_token'], session['access_token'], session['expires_in'] = gmail.get_authorization(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET)
+        # session['GOOGLE_REFRESH_TOKEN'] = session['refresh_token']
+
+    # send_mail('dnshelby712@gmail.com', 'dnshelby712@gmail.com',
+    #           'A mail from you from Python',
+    #           '<b>A mail from you from Python</b><br><br>' +
+    #           'So happy to hear from you!')
+    # email_client.send_message(email, to, message, password)
+    return render_template('index.html')
+
+@app.route('/done', methods=['GET', 'POST'])
+def done():
+    global RECIPIENT
+    code = request.args.get('code')
+    creds = oauth2.get_credentials(code, None)
+    user_info = oauth2.get_user_info(creds)
+    email_address = user_info.get('email')
+    gmail = build_service.build_service(creds)
+    message = quickstart.create_message("me", RECIPIENT, "Test subject", "Test body")
+    quickstart.send_message(gmail, "me", message)
     return render_template('index.html')
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=5001)
+    app.run(debug=True,host='192.168.0.17', port=5001,ssl_context='adhoc')
--- quickstart.py
+++ quickstart.py
@@ -7,43 +7,43 @@ import pickle
 from email.mime.text import MIMEText
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from googleapiclient import errors
-from googleapiclient.discovery import build
+from apiclient import errors
+from apiclient.discovery import build
 
-def get_service():
-    """Gets an authorized Gmail API service instance.
-
-    Returns:
-        An authorized Gmail API service instance..
-    """
-
-    # If modifying these scopes, delete the file token.pickle.
-    SCOPES = [
-        'https://www.googleapis.com/auth/gmail.readonly',
-        'https://www.googleapis.com/auth/gmail.send',
-    ]
-
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('gmail', 'v1', credentials=creds)
-    return service
+# def get_service():
+#     """Gets an authorized Gmail API service instance.
+#
+#     Returns:
+#         An authorized Gmail API service instance..
+#     """
+#
+#     # If modifying these scopes, delete the file token.pickle.
+#     SCOPES = [
+#         'https://www.googleapis.com/auth/gmail.readonly',
+#         'https://www.googleapis.com/auth/gmail.send',
+#     ]
+#
+#     creds = None
+#     # The file token.pickle stores the user's access and refresh tokens, and is
+#     # created automatically when the authorization flow completes for the first
+#     # time.
+#     if os.path.exists('token.pickle'):
+#         with open('token.pickle', 'rb') as token:
+#             creds = pickle.load(token)
+#     # If there are no (valid) credentials available, let the user log in.
+#     if not creds or not creds.valid:
+#         if creds and creds.expired and creds.refresh_token:
+#             creds.refresh(Request())
+#         else:
+#             flow = InstalledAppFlow.from_client_secrets_file(
+#                 'credentials.json', SCOPES)
+#             creds = flow.run_local_server(port=0)
+#         # Save the credentials for the next run
+#         with open('token.pickle', 'wb') as token:
+#             pickle.dump(creds, token)
+#
+#     service = build('gmail', 'v1', credentials=creds)
+#     return service
 
 def send_message(service, sender, message):
   """Send an email message.
