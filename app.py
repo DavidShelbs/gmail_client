8cbb698e4feea667e7d2b3140933963bfa2938fb
dnsnwb <dnsnwb@mst.edu>
2019-11-04 22:33:21 -0600

    cleaning up

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
-import quickstart
+import gmail_api
 import oauth2
 import build_service
 import os
-
-# session['RECIPIENT'] = session['MESSAGE'] = session['SUBJECT'] = ""
 
 app = Flask(__name__)
 
@@ -20,17 +18,6 @@ def spam():
         session['SUBJECT'] = request.form.get('subject')
         session['MESSAGE'] = request.form.get('message')
         return redirect(oauth2.get_authorization_url("dnshelby712@gmail.com", None))
-        # scope = "https://mail.google.com/"
-        # if GOOGLE_REFRESH_TOKEN is None:
-        #     return redirect(gmail.generate_permission_url(GOOGLE_CLIENT_ID, scope))
-            # session['refresh_token'], session['access_token'], session['expires_in'] = gmail.get_authorization(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET)
-            # session['GOOGLE_REFRESH_TOKEN'] = session['refresh_token']
-
-        # send_mail('dnshelby712@gmail.com', 'dnshelby712@gmail.com',
-        #           'A mail from you from Python',
-        #           '<b>A mail from you from Python</b><br><br>' +
-        #           'So happy to hear from you!')
-        # email_client.send_message(email, to, message, password)
     except:
         return render_template('index.html')
 
@@ -42,8 +29,8 @@ def done():
         user_info = oauth2.get_user_info(creds)
         email_address = user_info.get('email')
         gmail = build_service.build_service(creds)
-        message = quickstart.create_message("me", session['RECIPIENT'], session['SUBJECT'], session['MESSAGE'])
-        quickstart.send_message(gmail, "me", message)
+        message = gmail_api.create_message("me", session['RECIPIENT'], session['SUBJECT'], session['MESSAGE'])
+        gmail_api.send_message(gmail, "me", message)
         return render_template('index.html')
     except:
         return render_template('index.html')
